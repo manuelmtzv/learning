ab354ced9ceaf269bb5469d09a1c369740329b7c
Manuel Martínez <manuel.mtzv816@gmail.com>
2024-10-28 23:24:41 -0600

    refactor: update social-api configuration and add storage module

--- social-api/cmd/api/main.go
+++ social-api/cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"social-api/internal/env"
+	"social-api/internal/store"
 )
 
 func main() {
@@ -10,8 +11,11 @@ func main() {
 		addr: env.GetString("ADDR", ":8080"),
 	}
 
+	store := store.NewStorage(nil)
+
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
 
 	mux := app.mount()
