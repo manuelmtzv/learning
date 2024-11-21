4675464d7fd05e21abc4e21b727a0cecfbdc061b
manuelmtzv <manuel.mtzv816@gmail.com>
2024-11-21 16:31:35 -0600

    refactor: update broadcast example to use channels instead of condition variables

--- concurrency/broadcast/main.go
+++ concurrency/broadcast/main.go
@@ -10,43 +10,79 @@ type Button struct {
 }
 
 func main() {
-	button := Button{
-		Clicked: sync.NewCond(&sync.Mutex{}),
-	}
-
-	subscribe := func(c *sync.Cond, fn func()) {
+	subscribe := func(stream <-chan interface{}, fn func()) {
 		var goroutineRunning sync.WaitGroup
 		goroutineRunning.Add(1)
 
 		go func() {
 			goroutineRunning.Done()
-			c.L.Lock()
-			defer c.L.Unlock()
-			c.Wait()
+			<-stream
 			fn()
 		}()
 
 		goroutineRunning.Wait()
 	}
 
-	var clickRegistered sync.WaitGroup
-	clickRegistered.Add(3)
+	buttonClicked := make(chan interface{})
+	wg := sync.WaitGroup{}
 
-	subscribe(button.Clicked, func() {
+	wg.Add(3)
+
+	subscribe(buttonClicked, func() {
 		fmt.Println("Maximizing window.")
-		clickRegistered.Done()
+		wg.Done()
 	})
-
-	subscribe(button.Clicked, func() {
+	subscribe(buttonClicked, func() {
 		fmt.Println("Displaying annoying dialog box!")
-		clickRegistered.Done()
+		wg.Done()
 	})
-
-	subscribe(button.Clicked, func() {
+	subscribe(buttonClicked, func() {
 		fmt.Println("Mouse clicked.")
-		clickRegistered.Done()
+		wg.Done()
 	})
 
-	button.Clicked.Broadcast()
-	clickRegistered.Wait()
+	close(buttonClicked)
+	wg.Wait()
 }
+
+// func main() {
+// 	button := Button{
+// 		Clicked: sync.NewCond(&sync.Mutex{}),
+// 	}
+
+// 	subscribe := func(c *sync.Cond, fn func()) {
+// 		var goroutineRunning sync.WaitGroup
+// 		goroutineRunning.Add(1)
+
+// 		go func() {
+// 			goroutineRunning.Done()
+// 			c.L.Lock()
+// 			defer c.L.Unlock()
+// 			c.Wait()
+// 			fn()
+// 		}()
+
+// 		goroutineRunning.Wait()
+// 	}
+
+// 	var clickRegistered sync.WaitGroup
+// 	clickRegistered.Add(3)
+
+// 	subscribe(button.Clicked, func() {
+// 		fmt.Println("Maximizing window.")
+// 		clickRegistered.Done()
+// 	})
+
+// 	subscribe(button.Clicked, func() {
+// 		fmt.Println("Displaying annoying dialog box!")
+// 		clickRegistered.Done()
+// 	})
+
+// 	subscribe(button.Clicked, func() {
+// 		fmt.Println("Mouse clicked.")
+// 		clickRegistered.Done()
+// 	})
+
+// 	button.Clicked.Broadcast()
+// 	clickRegistered.Wait()
+// }
